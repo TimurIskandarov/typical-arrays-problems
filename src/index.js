768c5a399b442402fd0cae334bb096ab3f608d95
TimurIskandarov <tim-darov@rambler.ru>
2020-03-21 19:59:30 +0400

    feat: add min max avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length == 0) return 0;
+  let min = array[0];
+  for(let element of array){
+    min = element < min ? element : min;
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length == 0) return 0;
+  let max = array[0];
+  for(let element of array){
+    max = element > max ? element : max;
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length == 0) return 0;
+  return array.reduce((a,b) => a +=b) / array.length;
 }
